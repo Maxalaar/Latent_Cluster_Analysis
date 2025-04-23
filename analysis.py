df6c96e0c886a66990aae5c491349cbfd8c9a755
Maxalaar <maxalaar@gmail.com>
2025-04-23 16:21:16 +0200

    MOD : Brendan analysis

--- analysis.py
+++ analysis.py
@@ -6,6 +6,7 @@ from torchvision.transforms import ToTensor
 from architecture.autoencoder import Autoencoder
 from utilities.compare_reconstruction import compare_reconstruction
 from utilities.get_checkpoint_paths import get_checkpoint_paths
+from utilities.interactive_2d_image_plot import interactive_2d_image_plot
 from utilities.mean_adjusted_rand_index import mean_adjusted_rand_index
 from utilities.save_tsne import save_tsne
 from utilities.training_configuration import TrainingConfiguration
@@ -14,14 +15,16 @@ from utilities.tsne import tsne
 
 
 if __name__ == '__main__':
-    repository_path = Path('/home/malaarabiou/Programming_Projects/Pycharm_Projects/Latent_Cluster_Analysis/experiments/multilayer_clustering_autoencoder_800_epochs/model/version_3')
+    repository_path = Path('/home/malaarabiou/Programming_Projects/Pycharm_Projects/Latent_Cluster_Analysis/temporary/old_experiments/variational_autoencoder_10_minutes/model/version_4')
     checkpoint_paths = get_checkpoint_paths(repository_path)
     number_sample = 10_000
     cluster_number = 10
+
     use_tsne = True
     use_clustering = True
     use_adjusted_rand_index = True
     use_compare_reconstruction = False
+    use_interactive_2d_image_plot = False
 
     dataset_class = MNIST
     dataset = dataset_class(
@@ -65,6 +68,9 @@ if __name__ == '__main__':
             projection_2d = tsne(code)
             save_tsne(projection_2d, save_path, dataset_label, name='dataset_label')
             save_tsne(projection_2d, save_path, cluster_label, name='cluster_label')
+
+        if use_interactive_2d_image_plot:
+            interactive_2d_image_plot(projection_2d, samples, dataset_label)
 
         if use_adjusted_rand_index:
             cluster_label_list.append(cluster_label)
